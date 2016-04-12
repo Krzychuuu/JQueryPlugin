3c880f90723f992e2f079cd124294b50a9c95578
Krzysztof Ochędzan <jurasik@tlen.pl>
2016-04-12 11:37:02 +0200

    Error messages

--- jquery.formValidate.js
+++ jquery.formValidate.js
@@ -11,6 +11,7 @@
 		$(this).change(function ()
 		{
 			$(this).toggleClass("invalid", !pattern.test(this.value));
+			pattern.test(this.value) ? $(this).next("span").remove() : $(this).after("<span class='error' id='post'>Start with upper case</span>");
 			checkInputs(this);
 		});
 		
@@ -67,12 +68,32 @@
 		{
 			if($(this).hasClass('invalid') || !$(this).val()){
 				validated = false;
+				// throwError($(this));
 				return false;
 			}
 			validated = true;
+			removeErrors();
 		});
 
 		validated ? submit.attr('disabled',false) : submit.attr('disabled',true);
 	};
+
+	// var throwError = function(obj)
+	// {
+	// 	console.log($(obj));
+	// 	switch(obj)
+	// 	{
+	// 		case name:
+	// 			$(this).after("<span class='error' id='post'>Start with letter</span>");
+	// 		case surname:
+	// 			$(this).after("<span class='error' id='post'>Start with letter</span>");
+	// 		case email:
+	// 			$(this).after("<span class='error' id='post'>Not email format</span>");
+	// 		case password1:
+	// 			$(this).after("<span class='error' id='post'>Bad password</span>");
+	// 		case password2:
+	// 			$(this).after("<span class='error' id='post'>Passwords don't match</span>");
+	// 	}
+	// }
 
 })(jQuery);
